7f09781c11ff107008de4d372e3bb6ae8758744c
CrazyCrazy660 <CrazyCrazy6600@gmail.com>
2025-10-13 15:29:22 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+setInterval(function() {
+  fetch('/get_new_text')
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById('content').innerHTML = data;
+    });
+}, 5000);
